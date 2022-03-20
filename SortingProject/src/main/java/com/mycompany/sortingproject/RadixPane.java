384312bd12cfc3782a3d91fc5b6ea995f864bb71
boonewr <boonewr@guilford.edu>
2022-03-19 20:39:46 -0400

    Radix sort works!
    Array of ArrayLists say that 10 times fast!!

--- SortingProject/src/main/java/com/mycompany/sortingproject/RadixPane.java
+++ SortingProject/src/main/java/com/mycompany/sortingproject/RadixPane.java
@@ -1,6 +1,7 @@
 /* To do: make it go step by step :)
  */
 package com.mycompany.sortingproject;
+import java.util.ArrayList;
 import javafx.event.ActionEvent;
 
 /**
@@ -16,91 +17,6 @@ public class RadixPane extends ListPane {
     }
     
     /*
-    @Override
-    public void processSortButton(ActionEvent evt) {
-        /*
-        int max = MasterList.get(0);
-        System.out.println(MasterList.toString());
-        
-        for (int i = 0; i < MasterList.size(); i++) {
-            if (MasterList.get(i) > max) {
-                max = MasterList.get(i);
-            }
-        }
-        
-        ArrayList<Integer> counting = new ArrayList<Integer>();
-        for (int i = 0; i <= max; i++) {
-            counting.add(0);
-        }
-        
-        for (int i = 0; i < MasterList.size(); i++) {
-            counting.set(MasterList.get(i), counting.get(i) + 1);
-        }
-        
-        for (int i = 1; i < MasterList.size(); i++) {
-            counting.set(i, counting.get(i) + counting.get(i - 1));
-        }
-        
-        ArrayList<Integer> output = new ArrayList<Integer>();
-        for (int i = 0; i <= MasterList.size(); i++) {
-            output.add(0);
-        }
-        
-        for (int i = MasterList.size() - 1; i >= 0; i--) {
-            output.set(counting.get(MasterList.get(i)) - 1, MasterList.get(i));
-            counting.set(MasterList.get(i), counting.get(MasterList.get(i)) - 1);
-        }
-        
-        for (int i = 0; i < MasterList.size(); i++) {
-            MasterList.set(i, output.get(i));
-        }
-        
-     
-        int[] a = new int[MasterList.size()];
-        for (int i = 0; i < a.length; i++) {
-            a[i] = MasterList.get(i);
-        }
-        
-        int max = a[0];
-        for (int i = 0; i < a.length; i++) {
-            if (a[i] > max) {
-                max = a[i];
-            }
-        }
-        
-        int[] counting = new int[max + 1];
-
-        for (int i = 0; i < a.length; i++) {
-            counting[a[i]]++;
-        }
-
-        for (int i = 1; i < counting.length; i++) {
-            counting[i] += counting[i - 1];
-        }
-
-        int[] output = new int[a.length];
-
-        for (int i = a.length - 1; i >= 0; i--) {
-            output[counting[a[i]] - 1] = a[i];
-            counting[a[i]]--;
-        }
-
-        for (int i = 0; i < a.length; i++) {
-            a[i] = output[i];
-        }
-        
-        
-        for (int i = 0; i < a.length; i++) {
-            MasterList.set(i, a[i]);
-            updateListDisplay();
-            //TimeUnit.SECONDS.
-        }
-        
-        System.out.println("Sorted");
-        updateListDisplay();
-    }
-    */
-    
     @Override
     protected void processSortButton(ActionEvent evt) {
         switch (step) {
@@ -120,6 +36,75 @@ public class RadixPane extends ListPane {
                 System.out.println("What");
                 break;
         }
+    }
+    */    
+
+    int maxDigits;
+    int digits;
+    int power;
+    int bucketIndex;
+    int arrayIndex;
+    int temp;
+    
+    @Override
+    protected void processSortButton(ActionEvent evt) {        
+        int size = MasterList.size();
+        int[] array = new int[size];
+        ArrayList[] buckets = new ArrayList[10]; 
+        for (int i = 0; i < size; i++)
+            array[i] = MasterList.get(i);
+        
+        for (int i =0; i < 10; i++) {
+            ArrayList<Integer> buck = new ArrayList();
+            buckets[i] = buck;
+        }
+        
+        maxDigits = 0;
+        // Find the max length in digits
+        for (int i = 0; i < size; i++) {
+            if (getMaxDigits(array[i]) > maxDigits)
+                maxDigits = getMaxDigits(array[i]);
+        }
+        
+        power = 1;
+        for (int i = 0; i < maxDigits; i++) {
+            for (int j = 0; j < size; j++) {
+                bucketIndex = Math.abs(array[j] / power) % 10;
+                buckets[bucketIndex].add(array[j]);
+            }
+            arrayIndex = 0;
+            for (int j = 0; j < 10; j++) {
+                for (int n = 0; n < buckets[j].size(); n++) {
+                    System.out.println(buckets[j].get(n));
+                    array[arrayIndex] = (int)buckets[j].get(n);
+                    arrayIndex++;
+                }
+                
+            }
+            power *= 10;
+            
+            for (int j = 0; j < 10; j++) {
+                for (int n = 0; n < size; n++) {
+                    buckets[j].clear();
+                }
+            }
+        }
+        
+        for (int i = 0; i < size; i++) {
+            MasterList.set(i, array[i]);
+        }
+        updateListDisplay();
+    }
+    
+    private int getMaxDigits(int number) {
+        if (number == 0) return 1;
+        
+        digits = 0;
+        while (number != 0) {
+            digits++;
+            number /= 10;
+        }
+        return digits;
     }
     
     
