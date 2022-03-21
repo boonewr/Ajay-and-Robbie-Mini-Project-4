b4bbd89cb7f67334368cd7255cda38aee45764f8
ajdakoriya04 <92649641+ajdakoriya04@users.noreply.github.com>
2022-03-21 09:08:31 -0400

    Add files via upload

---
+++ SortingProject/src/main/java/com/mycompany/sortingproject/HomePane.java
@@ -0,0 +1,17 @@
+
+package com.mycompany.sortingproject;
+
+import java.util.ArrayList;
+import javafx.event.ActionEvent;
+
+/**
+ *
+ * @author Ajay
+ */
+public class HomePane extends ListPane {
+    public HomePane() {
+        setDescription("\"Please select a sorting method from the menu. Remember that some methods are faster than others!\"");
+        setName("Home Page");
+        sizeField.setVisible(false);
+    }
+}
